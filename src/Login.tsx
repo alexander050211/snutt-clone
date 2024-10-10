8b4bf1553e7fb0635b938f3bf558e8d124448d2a
working05 <106227265+working05@users.noreply.github.com>
2024-10-10 20:06:47 +0900

    login page design (#3)

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,7 @@
 import './reset.css';
 
+import styles from './Login.module.css';
+
 type Nickname = {
   nickname: string;
   tag: string;
@@ -14,15 +16,37 @@ const Login = ({
   const newNickname = { nickname: 'asdf', tag: 'todo' };
 
   return (
-    <>
-      <button
-        onClick={() => {
-          onLoginSuccess({ newNickname });
-        }}
-      >
-        Login
-      </button>
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <label className={styles.title}>로그인</label>
+        <div className={styles.inputContent}>
+          <label className={styles.text}>아이디</label>
+          <input
+            className={styles.inputBox}
+            type="text"
+            placeholder="아이디를 입력하세요"
+          />
+
+          <label className={styles.text}>비밀번호</label>
+          <input
+            className={styles.inputBox}
+            type="text"
+            placeholder="비밀번호를 입력하세요"
+          />
+          <label className={styles.additional}>
+            아이디 찾기 | 비밀번호 재설정
+          </label>
+          <button
+            className={styles.login}
+            onClick={() => {
+              onLoginSuccess({ newNickname });
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
