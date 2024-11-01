0518101fa98cda7a757b12937f3416e0cce63e63
knoj014 <jongkyung2005@gmail.com>
2024-11-02 02:57:47 +0900

    refresh, mypage
    파일 이름 정리, 이미지를 resources에 옮김
    로그인 중에 로그인 중 표시
    새로고침할 때 로그인 상태 유지
    mypage에서 로그아웃 가능
    mypage 디자인 해야 됨

--- src/Mypage.tsx
+++ src/Mypage.tsx
@@ -3,12 +3,29 @@ import './reset.css';
 import styles from './Mypage.module.css';
 import Navibar from './Navibar';
 
-const Mypage = () => {
-  //todo
+type Nickname = {
+  nickname: string;
+  tag: string;
+};
+
+type MypageProps = {
+  nickname: Nickname | undefined;
+  onLogout: () => void;
+};
+
+const Mypage = ({ nickname, onLogout }: MypageProps) => {
+  //todo: design
   return (
     <div className={styles.container}>
-      <div className={styles.tmpContent}>
+      <div className={styles.content}>
         <h1 className={styles.title}>Mypage todo</h1>
+        <h1 className={styles.title}>
+          {' '}
+          {nickname != null
+            ? `${nickname.nickname}#${nickname.tag}`
+            : 'Error'}{' '}
+        </h1>
+        <button onClick={onLogout}> 로그아웃 </button>
       </div>
       <Navibar />
     </div>
--- src/Navibar.tsx
+++ src/Navibar.tsx
@@ -2,15 +2,16 @@ import './reset.css';
 
 import { Link } from 'react-router-dom';
 
-import friends from './friends.svg';
-import lecturereview from './lecture_review.svg';
-import mypage from './mypage.svg';
 import styles from './Navibar.module.css';
-import search from './search.svg';
-import timetable from './timetable.svg';
+import friends from './resources/navibar_friends.svg';
+import lecturereview from './resources/navibar_lecture_review.svg';
+import mypage from './resources/navibar_mypage.svg';
+import search from './resources/navibar_search.svg';
+import timetable from './resources/navibar_timetable.svg';
 
 //선택된 메뉴가 검은색으로 칠해지는 건 구현해야 됨
 //검색, 강의평, 친구도 일단 시간표로 연결해 둠
+
 const Navibar = () => {
   return (
     <div className={styles.navibarFrame}>
@@ -18,13 +19,13 @@ const Navibar = () => {
         <Link to="/" className={styles.navibarButton}>
           <img src={timetable} alt="Timetable" />
         </Link>
-        <Link to="/" className={styles.navibarButton}>
+        <Link to="/search" className={styles.navibarButton}>
           <img src={search} alt="Search" />
         </Link>
-        <Link to="/" className={styles.navibarButton}>
+        <Link to="/review" className={styles.navibarButton}>
           <img src={lecturereview} alt="Lecture Review" />
         </Link>
-        <Link to="/" className={styles.navibarButton}>
+        <Link to="/friends" className={styles.navibarButton}>
           <img src={friends} alt="Friends" />
         </Link>
         <Link to="/mypage" className={styles.navibarButton}>
