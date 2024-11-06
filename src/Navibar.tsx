5c0651fbb5fce00d696415f7b66bdddaf248ec6a
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-11-06 14:25:09 +0900

    fixed + recategorized svg's

--- src/Navibar.tsx
+++ src/Navibar.tsx
@@ -1,19 +1,26 @@
 import './reset.css';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import styles from './Navibar.module.css';
 import friends from './resources/navibar_friends.svg';
 import lecturereview from './resources/navibar_lecture_review.svg';
-import mypage from './resources/navibar_mypage.svg';
+import mypageOff from './resources/navibar_mypage_off.svg';
+import mypageOn from './resources/navibar_mypage_on.svg';
 import search from './resources/navibar_search.svg';
-import timetable from './resources/navibar_timetable.svg';
+import timetableOff from './resources/navibar_timetable_off.svg';
+import timetableOn from './resources/navibar_timetable_on.svg';
 const Navibar = () => {
+  const location = useLocation();
+
   return (
     <div className={styles.navibarFrame}>
       <div className={styles.navibarContent}>
         <Link to="/" className={styles.navibarButton}>
-          <img src={timetable} alt="Timetable" />
+          <img
+            src={location.pathname === '/' ? timetableOn : timetableOff}
+            alt="Timetable"
+          />
         </Link>
         <Link to="/search" className={styles.navibarButton}>
           <img src={search} alt="Search" />
@@ -25,7 +32,10 @@ const Navibar = () => {
           <img src={friends} alt="Friends" />
         </Link>
         <Link to="/mypage" className={styles.navibarButton}>
-          <img src={mypage} alt="MyPage" />
+          <img
+            src={location.pathname === '/mypage' ? mypageOn : mypageOff}
+            alt="MyPage"
+          />
         </Link>
       </div>
     </div>
