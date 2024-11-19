68d5d6a6c5d72286feb56041e307783a4256d984
knoj014 <jongkyung2005@gmail.com>
2024-11-19 11:29:43 +0900

    lecturedetail implement
    lecturedetail 페이지 제작
    삭제할 때 한 번 더 확인하는 modal 추가

--- src/LectureDetail.tsx
+++ src/LectureDetail.tsx
@@ -1,7 +1,16 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
+import styles from './LectureDetail.module.css';
+import leftArrow from './resources/lecturedetail_leftarrow.svg';
 import { deleteLecture } from './utils/Functions';
 import type { Lecture } from './utils/Types';
+
+const WEEKDAYS = ['월', '화', '수', '목', '금', '토', '일'];
+
+const getWeekdayString = (day: number): string => {
+  return WEEKDAYS[day] ?? '';
+};
 
 type LocationState = {
   timetableId: string;
@@ -11,36 +20,135 @@ type LocationState = {
 const LectureDetail = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const state = location.state as LocationState;
+  const { timetableId, lecture } = location.state as LocationState;
+  const [showModal, setShowModal] = useState(false);
 
   const handleDelete = () => {
-    deleteLecture(state.timetableId, state.lecture._id)
+    setShowModal(true);
+  };
+
+  const confirmDelete = () => {
+    deleteLecture(timetableId, lecture._id)
       .then(() => {
         navigate('/');
       })
       .catch((err: unknown) => {
         console.error('Error: ', err);
       });
+    setShowModal(false);
   };
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        돌아가기
-      </button>
-      <h1>{state.lecture.course_title}</h1>
-      {/* todo */}
-      <button
-        onClick={() => {
-          handleDelete();
-        }}
-      >
-        강의 삭제
-      </button>
+    <div className={styles.container}>
+      <div className={styles.upperBar}>
+        <Link to="/" className={styles.timetableButton}>
+          <img src={leftArrow} />
+        </Link>
+        <div className={styles.pageTitle}>강의 상세 보기</div>
+      </div>
+
+      <div className={styles.contentContainer}>
+        <div className={styles.divider} />
+
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>강의명</div>
+          <div className={styles.contentText}>{lecture.course_title}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>교수</div>
+          <div className={styles.contentText}>{lecture.instructor}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>색상</div>
+          <div className={styles.contentText}>{lecture.colorIndex}</div>
+        </div>
+
+        <div className={styles.divider} />
+
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>학과</div>
+          <div className={styles.contentText}>{lecture.department}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>학년</div>
+          <div className={styles.contentText}>{lecture.academic_year}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>학점</div>
+          <div className={styles.contentText}>{lecture.credit}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>분류</div>
+          <div className={styles.contentText}>{lecture.classification}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>구분</div>
+          <div className={styles.contentText}>{lecture.category}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>강좌번호</div>
+          <div className={styles.contentText}>{lecture.course_number}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>분반번호</div>
+          <div className={styles.contentText}>{lecture.lecture_number}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>정원</div>
+          <div className={styles.contentText}>{lecture.quota}</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>비고</div>
+          <div className={styles.contentText}>{lecture.remark}</div>
+        </div>
+
+        <div className={styles.divider} />
+
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>시간 및 장소</div>
+        </div>
+        {lecture.class_time_json.map((classTime, index) => (
+          <React.Fragment key={index}>
+            <div className={styles.content}>
+              <div className={styles.contentTitle}>시간</div>
+              <div className={styles.contentText}>
+                {getWeekdayString(classTime.day)}({classTime.start_time}-
+                {classTime.end_time})
+              </div>
+            </div>
+            <div className={styles.content}>
+              <div className={styles.contentTitle}>장소</div>
+              <div className={styles.contentText}>{classTime.place}</div>
+            </div>
+          </React.Fragment>
+        ))}
+        <div className={styles.divider} />
+        <div className={styles.content} onClick={handleDelete}>
+          <div className={styles.deleteText}>삭제</div>
+        </div>
+      </div>
+
+      {showModal && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modal}>
+            <div className={styles.modalContent}>
+              <div className={styles.modalTitle}>강의 삭제</div>
+              <div className={styles.modalText}>강의를 삭제하시겠습니까?</div>
+              <div className={styles.modalButtons}>
+                <div
+                  onClick={() => {
+                    setShowModal(false);
+                  }}
+                  className={styles.modalButton}
+                >
+                  취소
+                </div>
+                <div onClick={confirmDelete}>확인</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
